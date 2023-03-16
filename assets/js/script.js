003853339e11ca66d05f3f56f12a22328b28a637
Jeff Zenko <jeff.zenko@gmail.com>
2023-03-16 00:12:20 -0400

    comments & workflow

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,17 @@
-var timer = document.getElementById("timer");
-const startQuiz = document.getElementById("start-quiz");
-const quiz = document.getElementById("quiz");
-const quizIntro = document.getElementById("quiz-intro");
-var initialTime = 10;
-const url= './assets/js/questions.json';
-const corsUrl = `https://cors-anywhere.herokuapp.com/${url}`;
+var timer = document.getElementById("timer"); //timer area
+const startQuiz = document.getElementById("start-quiz"); //start game button
+const quiz = document.getElementById("quiz"); //quiz area
+const quizIntro = document.getElementById("quiz-intro"); //quiz introduction area
+var initialTime = 10; //initial gametime. maybe make this variable for hard mode later?
+const url= './assets/js/questions.json'; //question bank - no cheating!
+const corsUrl = `https://cors-anywhere.herokuapp.com/${url}`; //json workaround?
 let currentScore; //initialize currentScore
 let highScores = {}; //initialize high scores to later be stored in an object with name:score as k:v
 
+
 function loadQuiz() {
     quizIntro.style.display = 'none';
-    fetch(url)
+    fetch(url) //found out how to do this promise on stackoverflow and youtube
     .then(res => res.json())
     .then(data => {
         console.log(data);
@@ -19,6 +20,25 @@ function loadQuiz() {
 
 }
 
+function gameStart( ) {
+
+
+}
+
+function answerSelected() {
+
+
+}
+
+
+function pageNextQuestion() {
+
+}
+
+function addToHighScore() {
+
+
+}
 
 quiz.onclick = () => {
     console.log('ready');
